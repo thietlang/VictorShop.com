b1fe18048856ee09ecafa02c15d123cde85657b6
thielang <xuanthuc123412@gmail.com>
2024-12-03 08:41:14 +0700

    add validate for sing up , sign in

--- login.js
+++ login.js
@@ -1,43 +1,176 @@
- // Đảm bảo thông báo không hiện khi trang mới tải
- document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById('customAlert').style.display = 'none';
-});
-
-function validateForm() {
-    var email = document.forms["registrationForm"]["email"].value;
-    var password = document.forms["registrationForm"]["password"].value;
-    var name = document.forms["registrationForm"]["name"].value;
-
-    // Kiểm tra họ và tên
-    if (!/^[a-zA-Z\sÀ-ỹ]{1,255}$/.test(name)) {
-        alert("Họ và tên không được chứa ký tự đặc biệt và không quá 255 ký tự");
-        return false;
-    }
-    // Kiểm tra cú pháp email
-    if (!/^\w+([\.-]?\w+)*@(gmail\.com|yahoo\.com|hotmail\.com|outlook\.com|aol\.com|icloud\.com|me\.com|protonmail\.com|yandex\.com|live\.com|qq\.com|163\.com)$/.test(email)) {
-        alert("Email phải có định dạng hợp lệ và đuôi phổ biến như @gmail.com, @yahoo.com, v.v.");
-        return false;
+// Đối tượng `Validator`
+function Validator(options) {
+    function getParent(element, selector) {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
     }
 
-    // Kiểm tra chiều dài mật khẩu
-    if (password.length <= 5 || password.length >= 255) {
-        alert("Mật khẩu phải dài ít nhất 6 kí tự và không quá 255 kí tự");
-        return false;
+    var selectorRules = {};
+
+    // Hàm thực hiện validate
+    function validate(inputElement, rule) {
+        var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
+        var errorMessage;
+
+        // Lấy ra các rules của selector
+        var rules = selectorRules[rule.selector];
+        
+        // Lặp qua từng rule & kiểm tra
+        // Nếu có lỗi thì dừng việc kiểm
+        for (var i = 0; i < rules.length; ++i) {
+            switch (inputElement.type) {
+                case 'radio':
+                case 'checkbox':
+                    errorMessage = rules[i](
+                        formElement.querySelector(rule.selector + ':checked')
+                    );
+                    break;
+                default:
+                    errorMessage = rules[i](inputElement.value);
+            }
+            if (errorMessage) break;
+        }
+        
+        if (errorMessage) {
+            errorElement.innerText = errorMessage;
+            getParent(inputElement, options.formGroupSelector).classList.add('invalid');
+        } else {
+            errorElement.innerText = '';
+            getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+        }
+
+        return !errorMessage;
     }
 
-    // Kiểm tra khoảng trắng mật khẩu
-    if (/\s/.test(password)) {
-        alert("Mật khẩu không được chứa khoảng trắng.");
-        return false;
+    // Lấy element của form cần validate
+    var formElement = document.querySelector(options.form);
+    if (formElement) {
+        // Khi submit form
+        formElement.onsubmit = function (e) {
+            e.preventDefault();
+
+            var isFormValid = true;
+
+            // Lặp qua từng rules và validate
+            options.rules.forEach(function (rule) {
+                var inputElement = formElement.querySelector(rule.selector);
+                var isValid = validate(inputElement, rule);
+                if (!isValid) {
+                    isFormValid = false;
+                }
+            });
+
+            if (isFormValid) {
+                // Trường hợp submit với javascript
+                if (typeof options.onSubmit === 'function') {
+                    var enableInputs = formElement.querySelectorAll('[name]');
+                    var formValues = Array.from(enableInputs).reduce(function (values, input) {
+                        
+                        switch(input.type) {
+                            case 'radio':
+                                values[input.name] = formElement.querySelector('input[name="' + input.name + '"]:checked').value;
+                                break;
+                            case 'checkbox':
+                                if (!input.matches(':checked')) {
+                                    values[input.name] = '';
+                                    return values;
+                                }
+                                if (!Array.isArray(values[input.name])) {
+                                    values[input.name] = [];
+                                }
+                                values[input.name].push(input.value);
+                                break;
+                            case 'file':
+                                values[input.name] = input.files;
+                                break;
+                            default:
+                                values[input.name] = input.value;
+                        }
+
+                        return values;
+                    }, {});
+                    options.onSubmit(formValues);
+                }
+                // Trường hợp submit với hành vi mặc định
+                else {
+                    formElement.submit();
+                }
+            }
+        }
+
+        // Lặp qua mỗi rule và xử lý (lắng nghe sự kiện blur, input, ...)
+        options.rules.forEach(function (rule) {
+
+            // Lưu lại các rules cho mỗi input
+            if (Array.isArray(selectorRules[rule.selector])) {
+                selectorRules[rule.selector].push(rule.test);
+            } else {
+                selectorRules[rule.selector] = [rule.test];
+            }
+
+            var inputElements = formElement.querySelectorAll(rule.selector);
+
+            Array.from(inputElements).forEach(function (inputElement) {
+               // Xử lý trường hợp blur khỏi input
+                inputElement.onblur = function () {
+                    validate(inputElement, rule);
+                }
+
+                // Xử lý mỗi khi người dùng nhập vào input
+                inputElement.oninput = function () {
+                    var errorElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
+                    errorElement.innerText = '';
+                    getParent(inputElement, options.formGroupSelector).classList.remove('invalid');
+                } 
+            });
+        });
     }
 
-    // Nếu tất cả các điều kiện đều vượt qua, hiển thị thông báo thành công
-    document.getElementById('customAlert').style.display = 'block';
-
-    // Ngăn chặn việc submit form để người dùng thấy thông báo thành công
-    return false;
 }
 
-function closeAlert() {
-    document.getElementById('customAlert').style.display = 'none';
+
+
+// Định nghĩa rules
+// Nguyên tắc của các rules:
+// 1. Khi có lỗi => Trả ra message lỗi
+// 2. Khi hợp lệ => Không trả ra cái gì cả (undefined)
+Validator.isRequired = function (selector, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value ? undefined :  message || 'Vui lòng nhập Username'
+        }
+    };
+}
+
+Validator.isEmail = function (selector, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+            return regex.test(value) ? undefined :  message || 'Trường này phải là email';
+        }
+    };
+}
+
+Validator.minLength = function (selector, min, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value.length >= min ? undefined :  message || `Vui lòng nhập tối thiểu ${min} kí tự`;
+        }
+    };
+}
+
+Validator.isConfirmed = function (selector, getConfirmValue, message) {
+    return {
+        selector: selector,
+        test: function (value) {
+            return value === getConfirmValue() ? undefined : message || 'Giá trị nhập vào không chính xác';
+        }
+    }
 }
