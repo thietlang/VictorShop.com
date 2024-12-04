cbfc66b4481d20e68fd6fad952ea6a1f76320062
thielang <xuanthuc123412@gmail.com>
2024-12-04 23:11:38 +0700

    update validate sign in and sign up

--- login.js
+++ login.js
@@ -95,6 +95,7 @@ function Validator(options) {
                     }, {});
                     options.onSubmit(formValues);
                 }
+                
                 // Trường hợp submit với hành vi mặc định
                 else {
                     formElement.submit();
@@ -133,6 +134,53 @@ function Validator(options) {
 }
 
 
+// Toast function
+function toast({ title = "", message = "", type = "info", duration = 3000 }) {
+    const main = document.getElementById("toast");
+    if (main) {
+      const toast = document.createElement("div");
+  
+      // Auto remove toast
+      const autoRemoveId = setTimeout(function () {
+        main.removeChild(toast);
+      }, duration + 1000);
+  
+      // Remove toast when clicked
+      toast.onclick = function (e) {
+        if (e.target.closest(".toast__close")) {
+          main.removeChild(toast);
+          clearTimeout(autoRemoveId);
+        }
+      };
+  
+      const icons = {
+        success: "fas fa-check-circle",
+        info: "fas fa-info-circle",
+        warning: "fas fa-exclamation-circle",
+        error: "fas fa-exclamation-circle"
+      };
+      const icon = icons[type];
+      const delay = (duration / 1000).toFixed(2);
+  
+      toast.classList.add("toast", `toast--${type}`);
+      toast.style.animation = `slideInLeft ease .3s, fadeOut linear 1s ${delay}s forwards`;
+  
+      toast.innerHTML = `
+                      <div class="toast__icon">
+                          <i class="${icon}"></i>
+                      </div>
+                      <div class="toast__body">
+                          <h3 class="toast__title">${title}</h3>
+                          <p class="toast__msg">${message}</p>
+                      </div>
+                      <div class="toast__close">
+                          <i class="fas fa-times"></i>
+                      </div>
+                  `;
+      main.appendChild(toast);
+    }
+  }
+  
 
 // Định nghĩa rules
 // Nguyên tắc của các rules:
@@ -142,7 +190,7 @@ Validator.isRequired = function (selector, message) {
     return {
         selector: selector,
         test: function (value) {
-            return value ? undefined :  message || 'Vui lòng nhập Username'
+            return value ? undefined :  message || 'Vui lòng nhập trường này'
         }
     };
 }
